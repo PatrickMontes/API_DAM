4201c3086ba7aa8078d7020803edf8f73f61c739
PatrickMontes <alexandermontes30p@hotmail.com>
2024-06-26 22:28:35 -0500

    Creacion de Proyecto

---
+++ src/main/java/pe/edu/cibertec/bembosapi/BembosApiApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.bembosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BembosApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BembosApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/cibertec/bembosapi/BembosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.bembosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BembosApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

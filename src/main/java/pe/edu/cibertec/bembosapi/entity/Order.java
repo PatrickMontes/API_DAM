12b3290fef7bc9520795688b64a52fa19f991d9f
PatrickMontes <alexandermontes30p@hotmail.com>
2024-07-01 20:43:41 -0500

    Error Corregido y Swagger Implementado

--- src/main/java/pe/edu/cibertec/bembosapi/entity/Order.java
+++ src/main/java/pe/edu/cibertec/bembosapi/entity/Order.java
@@ -1,31 +1,29 @@
 package pe.edu.cibertec.bembosapi.entity;
 
 import java.sql.Date;
-import java.util.List;
+import java.util.Set;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "pedido")
 public class Order {
-	@Id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-	@Column(name = "fechaCompra")
-	private Date fechaCompra;
-	@Column(name = "montoTotal")
+
+    @Column(name = "fechaCompra")
+    private Date fechaCompra;
+
+    @Column(name = "montoTotal")
     private Double montoTotal;
+
     @OneToOne
-    @JoinColumn(name="cliente_id")
+    @JoinColumn(name = "cliente_id")
     private Client cliente_id;
-    List<OrderDetails> listDetail;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<OrderDetails> listDetail;
 }
--- src/main/java/pe/edu/cibertec/bembosapi/entity/OrderDetails.java
+++ src/main/java/pe/edu/cibertec/bembosapi/entity/OrderDetails.java
@@ -1,30 +1,27 @@
 package pe.edu.cibertec.bembosapi.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "detallepedido")
 public class OrderDetails {
-	@Id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-	@Column(name = "cantidad")
-	private int cantidad;
-	@Column(name = "precioUni")
+
+    @Column(name = "cantidad")
+    private int cantidad;
+
+    @Column(name = "precioUni")
     private Double precioUni;
-    //@OneToOne
-    @JoinColumn(name="idPedido")
-    private Order idPedido;
+
+    @ManyToOne
+    @JoinColumn(name = "idPedido")
+    private Order order; // Renombrado de idPedido a order
+
     @OneToOne
-    @JoinColumn(name="idProducto")
+    @JoinColumn(name = "idProducto")
     private Product idProducto;
 }
